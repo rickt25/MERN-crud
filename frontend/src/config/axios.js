62298abc034a0fc86b56eaf5afbcc8a2a2b6730f
rickt25 <derricktio36@gmail.com>
2022-07-03 15:05:37 +0700

    Config Axios

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,35 +1,48 @@
 import axios from "axios";
 
 const axiosApi = axios.create({
-  baseURL: 'http://localhost:5001/'
+  baseURL: 'http://localhost:5001/',
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
-// instance.interceptors.request.use(
-//   config => {
-//     if (!config.headers.Authorization) {
-//       const user = JSON.parse(localStorage.getItem("user"));
-//       if (user.token) {
-//         config.headers.Authorization = `Bearer ${user.token}`;
-//       }
-//     }
+const refreshAccessToken = async () => {
+  try{
+    const response = await axiosApi.get("token");
+    console.log(response);
+    return response;
+  }catch(error){
+    console.log(error);
+  }
+}
 
-//     return config;
-//   },
-//   error => Promise.reject(error)
-// );
+axiosApi.interceptors.request.use(
+  config => {
+    if (!config.headers.Authorization) {
+      const user = JSON.parse(localStorage.getItem("user"));
+      if (user && user.token) {
+        config.headers.Authorization = `Bearer ${user.token}`;
+      }
+    }
 
-// instance.interceptors.response.use(
-//   (response) => {
-//   return response
-// }, async function (error) {
-//   const originalRequest = error.config;
-//   if (error.response.status === 403 && !originalRequest._retry) {
-//     originalRequest._retry = true;
-//     const access_token = await refreshAccessToken();            
-//     axios.defaults.headers.common['Authorization'] = 'Bearer ' + access_token;
-//     return axiosApiInstance(originalRequest);
-//   }
-//   return Promise.reject(error);
-// });
+    return config;
+  },
+  error => Promise.reject(error)
+);
+
+axiosApi.interceptors.response.use(
+  (response) => {
+    return response
+  }, async function (error) {
+  const originalRequest = error.config;
+  if (error.response.status === 403 && !originalRequest._retry) {
+    originalRequest._retry = true;
+    const access_token = await refreshAccessToken();            
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + access_token;
+    return axiosApi(originalRequest);
+  }
+  return Promise.reject(error);
+});
 
 export default axiosApi;
